d5a507b033d66b3e99ac2fdd640128b650b9a03a
LaCobble <71151090+LaCobble@users.noreply.github.com>
2022-03-17 18:11:49 +0100

    Update ApplicationException.java

--- src/ApplicationException.java
+++ src/ApplicationException.java
@@ -1,3 +1,9 @@
+//    ______      __    __    __
+//   / ____/___  / /_  / /_  / /__
+//  / /   / __ \/ __ \/ __ \/ / _ \
+// / /___/ /_/ / /_/ / /_/ / /  __/
+// \____/\____/_.___/_.___/_/\___/
+
 public  class ApplicationException  extends Exception {
    
     public ApplicationException (String s) {
